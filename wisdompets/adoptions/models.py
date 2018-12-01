6b88e1bb8b51488750ee4e11885d0db7006cc825
liberbell <liberbell@gmail.com>
2018-12-01 16:23:54 +0900

    models6

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -11,6 +11,7 @@ class Pet(models.Model):
     sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
     submission_data = models.DateTimeField()
     age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
