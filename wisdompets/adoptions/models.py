bc9d0075b92faff2992ea48f44be7a2c55fb737d
liberbell <liberbell@gmail.com>
2018-12-01 16:16:22 +0900

    models3

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Pet(models.Model):
     name = models.CharField(max_length=100)
     submitter = models.CharField(max_length=100)
-    species = models.CharField(max_length=100)
-    bread = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    bread = models.CharField(max_length=30, blank=True)
