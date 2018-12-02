5805c84c30d548283b43871317a63dd769132319
liberbell <liberbell@gmail.com>
2018-12-02 13:33:31 +0900

    models1

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -6,7 +6,7 @@ class Pet(models.Model):
     name = models.CharField(max_length=100)
     submitter = models.CharField(max_length=100)
     species = models.CharField(max_length=30)
-    bread = models.CharField(max_length=30, blank=True)
+    breed = models.CharField(max_length=30, blank=True)
     descrption = models.TextField()
     sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
     submission_date = models.DateTimeField()
