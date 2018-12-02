8df5148b30f7a676ed11bc3f4d7bebf0feee0f9b
liberbell <liberbell@gmail.com>
2018-12-02 14:10:34 +0900

    url3

--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
 
 from adoptions import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('^$', views.home, name='home'),
-    path('^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
+    url(r'^$', views.home, name='home'),
+    url(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
 ]
