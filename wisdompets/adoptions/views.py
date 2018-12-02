23ba0a6f384a0aaa41621ebfc1361d16692d64d5
liberbell <liberbell@gmail.com>
2018-12-02 14:15:16 +0900

    view4

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Pet
 # Create your views here.
 def home(request):
-    return HttpResponse('<p>home view</p>')
+    pets = Pet.objects.all()
+    # return HttpResponse('<p>home view</p>')
+    return render(request)
 
 def pet_detail(request, id):
     return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
